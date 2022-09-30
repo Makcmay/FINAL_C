74ffd29c4c373c69a652d31054225aa9a1402a22
MakcMay <makcmay@gmail.com>
2022-09-30 10:52:03 +0300

    add Task

---
+++ TASK/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите массив символов через пробел");
+string stroka = Console.ReadLine();
+
+string[] array = stroka.Split(" ").ToArray();
+
+if (stroka.Length != 0)
+{
+    string[] result = new string[array.Length];
+    int size = 0;
+
+    foreach (var elemen in array)
+    {
+        if (elemen.Length <= 3)
+        {
+            result[size] = elemen;
+            size++;
+        }
+    }
+    Console.WriteLine(string.Join(Environment.NewLine, result, 0, size));
+    Console.ReadKey(true);
+}
+else Console.WriteLine($"Вы ничего не ввели! Запустите программу заново.");
